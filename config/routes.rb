2810cf3d5e3084162544c09c7a5cc2fb53d11f36
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-12-18 04:36:55 -0800

    Created nested routes for groups and events

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :events
-  resources :groups
+
+  resources :groups do 
+    resources :events, only: [:new, :show]
+  end
 
   devise_for :users, path: '', path_names: { sign_up: 'signup', sign_in: 'login', sign_out: 'logout' }, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
