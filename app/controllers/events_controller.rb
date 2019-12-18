85fa3bd1e8b0b6b03be9f2b63b988d356a67e7cb
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-12-18 04:26:56 -0800

    Added functionality of editing/deleting events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -25,6 +25,26 @@ class EventsController < ApplicationController
     end
   end
 
+  def edit 
+  end
+
+  def update
+    @event.update(event_params)
+
+    if @event.save
+      flash[:notice] = "You've successfully updated your event"
+      redirect_to event_path(@event)
+    else
+      flash[:alert] = "There were some errors updating your event"
+      render :edit
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_path
+  end
+
   private
 
     def event_params
