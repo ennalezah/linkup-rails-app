3928c53965b043bfdd25d6574e234b4c0e943a09
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-12-17 18:09:27 -0800

    Customized signup, login, and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_up: 'signup', sign_in: 'login', sign_out: 'logout' }
   root 'welcome#index'
 end
