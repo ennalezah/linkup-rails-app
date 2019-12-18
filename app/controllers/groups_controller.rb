4f29b693bb5235ebfd72e90af5b7fd1151d8c74d
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-12-17 23:27:32 -0800

    Added functionality of creating a new group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,43 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:show, :edit, :update, :destroy]
+
+  def index 
+    @groups = Group.all
+  end
+
+  def show
+  end
+
+  def new
+    @group = Group.new(creator: current_user.name)
+  end
+
+  def create 
+    @group = Group.new(group_params)
+
+    if @group.save
+      redirect_to group_path(@group)
+    else
+      render 'new'
+    end
+  end
+
+  def edit 
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def group_params
+      params.require(:group).permit(:name, :description, :city, :state, :creator)
+    end
+
+    def set_group
+      @group = Group.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :groups
-  devise_for :users, path: '', path_names: { sign_up: 'signup', sign_in: 'login', sign_out: 'logout' }, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
+  devise_for :users, path: '', path_names: { sign_up: 'signup', sign_in: 'login', sign_out: 'logout' }, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root 'welcome#index'
 end
