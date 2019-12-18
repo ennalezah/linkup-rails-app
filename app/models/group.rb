7e7a44f72e41e1402c83160b7331c8432aa2f3e1
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-12-17 23:01:03 -0800

    Added validations to Group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,13 @@
 class Group < ApplicationRecord
+  before_validation :make_city_titlecase
+
+  validates :name, :description, :city, :state, presence: { message: "Cannot be left blank" } 
+
+  validates :city, format: { with: /\A[A-Za-z\s]+\z/, message: "Can only contain letters" }
+
+  private 
+  
+    def make_city_titlecase
+      self.city = self.city.titlecase
+    end
 end
