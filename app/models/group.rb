cf467326ef108db4eff64f892dda773502d9e3a6
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-12-19 01:21:57 -0800

    Added functionality of destroying dependent models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   ## Associations
-  has_many :events
-  has_many :memberships
+  has_many :events, dependent: :destroy
+  has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
 
   ## Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
 
   ## Associations
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :groups, through: :memberships
 
 
