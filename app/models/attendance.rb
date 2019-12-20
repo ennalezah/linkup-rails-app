f9dc5e16f9383f466073340643608e54091d33ba
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-12-20 00:00:49 -0800

    Added styling for navbar and links

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,7 +3,7 @@ class Attendance < ApplicationRecord
   belongs_to :event
 
   def self.add_user_to_group(attendance)
-    # if user attends event but doesn't have membership with event group, it will add user to group
+    # if user attends event but doesn't have membership with event group, it will add user to that group
     event_group = Event.find(attendance.event_id).group
     user = User.find(attendance.user_id)
 
