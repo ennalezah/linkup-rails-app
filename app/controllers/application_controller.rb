c3c6d69ce29354fbb4d4a46e6cc764b920d6058e
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-12-20 20:01:37 -0800

    Added styling to login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include DefaultPageContent
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || user_path(resource)
+  end
 end
