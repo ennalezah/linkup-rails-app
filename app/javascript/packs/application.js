c6ff7dde051ea0dbeebc3195abb39ee579748b06
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-12-21 07:55:06 -0800

    Finished styling events#new form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,25 @@ require("@rails/activestorage").start()
 require("channels")
 require ("bulma-extensions")
 
+// Initialize all input of date type.
+const calendars = bulmaCalendar.attach('[type="date"]', options);
+
+// Loop on each calendar initialized
+calendars.forEach(calendar => {
+	// Add listener to date:selected event
+	calendar.on('date:selected', date => {
+		console.log(date);
+	});
+});
+
+// To access to bulmaCalendar instance of an element
+const element = document.querySelector('#my-element');
+if (element) {
+	// bulmaCalendar instance is available as element.bulmaCalendar
+	element.bulmaCalendar.on('select', datepicker => {
+		console.log(datepicker.data.value());
+	});
+}
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
