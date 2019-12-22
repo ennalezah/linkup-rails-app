4b1dddc17f9800c8cff350d78ce26ee0da8c849e
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-12-22 03:42:21 -0800

    Updated version of Font Awesome icons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include DefaultPageContent
-
-  def after_sign_in_path_for(resource)
-    stored_location_for(resource) || user_path(resource)
-  end
 end
