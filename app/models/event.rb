5788f08268da7b31696b1162ca885e9ff74df21d
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-01-02 17:45:29 -0800

    Added event validator to check for future date

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,19 +1,23 @@
 class Event < ApplicationRecord
   ## Associations
   belongs_to :group
-
   has_many :attendances, dependent: :destroy
   has_many :users, through: :attendances
 
+  ## Validations
+  validate :date_cannot_be_in_the_past
+  validates_presence_of :name, :details, :start_time, :end_time, :location, :city, :state, :organizer, :group_id
 
   ## Scope
   default_scope { order(date: :desc) }
   scope :upcoming, lambda { where('date >= ?', Time.now) } # lambda reavluates scope each time it's called
   scope :past, lambda { where('date < ?', Time.now) }
 
-
-  ## Validations
-  validates_presence_of :name, :details, :date, :start_time, :end_time, :location, :city, :state, :organizer, :group_id
-  before_validation :make_city_titlecase
-  # validates :city, format: { with: /[A-Za-z\-\'\.]+/x, message: "Cannot contain numbers or special characters" }
+  private 
+  
+  def date_cannot_be_in_the_past
+    if date.present? && date < Date.today
+      errors.add(:date, "<br>must select a future date".html_safe)
+    end
+  end
 end
